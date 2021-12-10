b14ff6ffaea6821d578a7ccda56b8f654e7edf5a
Fernando Borel <fernandomenezesborel@gmail.com>
2021-12-10 16:45:56 -0300

    questões funcionando

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,8 @@
 // dados iniciais
 let currentQuestion = 0;
+let correctAnswer = 0;
+
+
 
 showQuestion();
 
@@ -10,12 +13,36 @@ function showQuestion() {
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion];
 
+        // mostrar questões
         document.querySelector('.scoreArea').style.display = 'none';
-        document.querySelector('.options').style.display = 'block';
+        document.querySelector('.questionArea').style.display = 'block';
 
         document.querySelector('.question').innerHTML = q.question;
-        document.querySelector('.options').innerHTML = '';
+        
+        //mostrar respostas
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        //selecionando as respostas
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        })
     } else {
         // sem questões.
     }
+}
+
+//função para selecionar as respostas
+function optionClickEvent(e) {
+    let clickedOption = parseInt(e.target.getAttribute('data-op'));
+
+    if(questions[currentQuestion].answer === clickedOption) {
+        correctAnswer++;
+    }
+
+    currentQuestion++;
+    showQuestion();
 }
